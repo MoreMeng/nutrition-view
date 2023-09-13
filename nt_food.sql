a7eef75b974c533b6da73058fcc1c909a7cc36ba
Thanikul Sriuthis <moremeng@dv4.biz>
2023-09-13 14:14:52 +0700

    nutrition angthong hospital

--- nt_food.sql
+++ nt_food.sql
@@ -1,40 +1,24 @@
 -- DROP VIEW "public"."nt_food";
 -- CREATE VIEW "public"."nt_food" AS
-SELECT
-    m.ipt_food_menu_id AS ID,
-    m.an AS AN,
-    m.order_date AS FOOD_DATE,
-    l.NAME AS FOOD_TIME,
-    CASE
-        WHEN s.nutrition_type IN ('7', '8', '9') THEN 'พิเศษ'
-        WHEN s.nutrition_type = '6' THEN ''
-        ELSE 'ธรรมดา'
-    END as "FOOD_SET",
-    s.NAME AS FOOD_TYPE,
-    CASE
-        WHEN s.nutrition_type = '6' THEN 'งดอาหาร'
-        ELSE ''
-    END as "FOOD_STATUS",
-    CASE
-        WHEN s.nutrition_type IN ('1', '7') THEN 'ธรรมดา'
-        WHEN s.nutrition_type IN ('2', '8') THEN 'อ่อน'
-        WHEN s.nutrition_type IN ('3', '9') THEN 'เหลว'
-    END as "FOOD_EXTRA"
-FROM
-    ipt_food_menu m,
-    ipt i,
-    meal l,
-    ward w,
-    nutrition_items s,
-    patient p,
-    iptadm d
-WHERE
-    m.an = i.an
-    AND i.ward = w.ward
-    AND m.meal = l.meal
-    AND i.hn = p.hn
-    AND d.an = i.an
-    AND m.nutrition_items_id = s.nutrition_items_id
-    AND i.dchdate IS NULL
+SELECT ordd.nutrition_food_ord_detail_id "ID",
+    ord.an "AN",
+    ord.nutrition_food_ord_date "FOOD_DATE",
+    fd.nutrition_food_day_name "FOOD_TIME",
+    st.nutrition_food_sub_type_name "FOOD_SET",
+    fdd.nutrition_food_name "FOOD_TYPE",
+    CASE WHEN ordd.nutrition_food_food_chk = 'Y' THEN 'งดอาหาร' ELSE '' END "FOOD_STATUS",
+    CONCAT (
+        ordd.food_disease,
+        CASE WHEN COALESCE(ordd.food_disease, '') <> '' AND COALESCE(sp.nutrition_food_supply_name, '') <> '' THEN ' | ' ELSE '' END,
+        sp.nutrition_food_supply_name,
+        CASE WHEN COALESCE(ordd.food_disease, '') <> '' AND COALESCE(sp.nutrition_food_supply_name, '') <> '' AND COALESCE(ordd.nutrition_food_ord_detail, '') <> '' THEN ' | ' ELSE '' END,
+        ordd.nutrition_food_ord_detail
+        ) "FOOD_EXTRA"
+FROM nutrition_food_ord ord
+LEFT JOIN nutrition_food_ord_detail ordd ON ord.nutrition_food_ord_id = ordd.nutrition_food_ord_id
+LEFT JOIN nutrition_food_day fd ON ordd.nutrition_food_day_id = fd.nutrition_food_day_id
+LEFT JOIN nutrition_food_sub_type st ON ordd.nutrition_food_sub_type_id = st.nutrition_food_sub_type_id
+LEFT JOIN nutrition_food fdd ON ordd.nutrition_food_id = fdd.nutrition_food_id
+LEFT JOIN nutrition_food_supply sp ON ordd.nutrition_food_supply_id = sp.nutrition_food_supply_id
 -- DEBUG --
 -- LIMIT 100
